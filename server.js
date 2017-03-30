a6dca4619f49b48874fb62293e068d9d212c7e71
Sohom Bhattacharya <sohom95@yahoo.com>
2017-03-30 00:06:05 -0400

    created connection to db

--- server.js
+++ server.js
@@ -1,18 +1,47 @@
-var express    = require('express');        
-var app        = express();                 
+var express = require('express');        
+var app = express();                 
 var bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+var mysql = require('mysql');       
 var port = process.env.PORT || 8080;        
-var router = express.Router();              
+var router = express.Router();      
+var connection = mysql.createConnection({
+  host     : 'sql9.freemysqlhosting.net',
+  user     : 'sql9161597',
+  password : 'YPk6CC3RBb',
+  database : 'sql9161597'
+});
+connection.connect(function(err){
+    if(!err) {
+        console.log("Connected to DB");    
+        var server = app.listen(port, function(){
+        var enableCORS = function(req, res, next) {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+            // intercept OPTIONS method
+            if ('OPTIONS' == req.method) {
+              res.send(200);
+            }
+            else {
+              next();
+            }
+        };
+
+
+            // enable CORS!
+            app.use(enableCORS);        
+            app.use(bodyParser.urlencoded({ extended: true }));
+            app.use(bodyParser.json());         
+            app.use('/api', router); 
+        });        
+    } else {
+        console.log("Error connecting database");    
+    }
+});
 
 router.get('/', function(req, res) {
     res.json({ message: 'WebApp api' });   
 });
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
